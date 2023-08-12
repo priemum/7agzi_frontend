b1f8748675f7a71ed945d396c55f8973e8eb8b8b
Ahmed Hamouda <ahmedabdelrazzak1001010@gmail.com>
2023-08-12 13:37:59 -0700

    Added

--- src/auth/Forgot.js
+++ src/auth/Forgot.js
@@ -22,22 +22,22 @@ const Forgot = ({ language }) => {
 		setValues({ ...values, [name]: event.target.value });
 	};
 
-	// Check if the input is a valid email or phone number
-	let username;
-	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-	const phonePattern = /^\d+$/;
-
-	if (emailPattern.test(email)) {
-		username = email; // It's an email
-	} else if (phonePattern.test(email)) {
-		username = email; // It's a phone number
-	} else {
-		setValues({ ...values, error: true, loading: false });
-		return toast.error("Please fill in the correct phone or email");
-	}
-
 	const clickSubmit = (event) => {
 		event.preventDefault();
+		// Check if the input is a valid email or phone number
+		let username;
+		const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+		const phonePattern = /^\d+$/;
+
+		if (emailPattern.test(email)) {
+			username = email; // It's an email
+		} else if (phonePattern.test(email)) {
+			username = email; // It's a phone number
+		} else {
+			setValues({ ...values, error: true, loading: false });
+			return toast.error("Please fill in the correct phone or email");
+		}
+
 		setValues({ ...values, buttonText: "Submitting" });
 		axios({
 			method: "PUT",
@@ -132,7 +132,6 @@ const Forgot = ({ language }) => {
 				{chosenLanguage === "Arabic"
 					? passwordForgotFormArabic()
 					: passwordForgotForm()}
-				{}
 			</div>
 		</FogotWrapper>
 	);
